8ee2044f6837e37f47dc3d2935803dba5a622737
skilur1983 <skilur2009@gmail.com>
2023-05-11 17:27:10 +0300

    Adding Tests

---
+++ src/test/java/com/mycompany/blog/UserTest.java
@@ -0,0 +1,30 @@
+package com.mycompany.blog;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @org.junit.jupiter.api.Test
+    void getEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPost() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPost() {
+    }
+}
